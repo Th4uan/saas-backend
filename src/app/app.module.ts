d703dc161db325b3dd2197066badd90a5261b1b7
Thauan <dev.thauan.rodrigues@gmail.com>
2025-03-31 16:01:57 -0300

    chore: initialized project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'user',
+      password: 'teste',
+      database: 'teste',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
