b712ccf1bec83c409f1e8c8c18920f4c9cf89a01
Thauan <dev.thauan.rodrigues@gmail.com>
2025-04-05 20:31:19 -0300

    build: added nvmrc to change the version in deploy

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
-import { CACHE_MANAGER } from '@nestjs/cache-manager';
-import { Cache } from 'cache-manager';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,11 +13,8 @@ async function bootstrap() {
   );
   app.use(cookieParser());
 
-  const cache = app.get<Cache>(CACHE_MANAGER);
+  console.log(process.version);
 
-  await cache.set('ninja_key', 'salvo_via_nest', 120);
-  const valor = await cache.get('ninja_key');
-  console.log('⚡ Valor salvo:', valor);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
