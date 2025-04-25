58a0b5b0a3793e46ad9270e1590f86d45aebcb29
Thauan Rodrigues <devthauan@MacBook-Air-de-Thauan.local>
2025-04-25 11:28:39 -0300

    feat: was create route to get all doctos and create a doctor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,6 +7,7 @@ import {
   Get,
   Param,
   UseGuards,
+  Query,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -17,11 +18,13 @@ import {
   ApiBody,
   ApiCookieAuth,
   ApiParam,
+  ApiQuery,
   ApiResponse,
   ApiTags,
 } from '@nestjs/swagger';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { UserRoleEnum } from './enums/user-role.enum';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @ApiTags('User')
 @UseGuards(AuthTokenGuard)
@@ -64,10 +67,56 @@ export class UserController {
 
     return user;
   }
-  // @Get()
-  // findAll() {
-  //   return this.userService.findAll();
-  // }
+
+  @ApiResponse({ status: 201, description: 'Doctor created successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid Data' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiCookieAuth('jwt')
+  @Roles(UserRoleEnum.Admin, UserRoleEnum.Doctor)
+  @Post('doctor')
+  async createDoctor(@Body() createUserDto: CreateUserDto) {
+    if (!createUserDto) {
+      throw new HttpException('Invalid Data', HttpStatus.BAD_REQUEST);
+    }
+
+    const user = await this.userService.createDoctor(createUserDto);
+
+    if (!user) {
+      throw new HttpException('Invalid Data', HttpStatus.BAD_REQUEST);
+    }
+
+    return user;
+  }
+
+  @ApiResponse({ status: 201, description: 'Doctor getted successfully' })
+  @ApiResponse({ status: 400, description: 'Invalid Data' })
+  @ApiResponse({ status: 403, description: 'Forbidden' })
+  @ApiQuery({
+    name: 'offset',
+    required: false,
+    description: 'Page number',
+    type: Number,
+    example: 1,
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    description: 'Number of items per page',
+    type: Number,
+    example: 10,
+  })
+  @Get('doctors')
+  async findAllDoctors(@Query() pagination: PaginationDto) {
+    if (!pagination) {
+      throw new HttpException('Invalid Data', HttpStatus.BAD_REQUEST);
+    }
+
+    if (pagination.limit < 1) {
+      throw new HttpException('Invalid Data', HttpStatus.BAD_REQUEST);
+    }
+    return this.userService.findAllDoctors(pagination);
+  }
 
   @ApiResponse({ status: 200, description: 'User found' })
   @ApiResponse({ status: 400, description: 'Invalid Id' })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,16 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { HashingService } from 'src/auth/hashing/hashing.service';
 import { UserRoleEnum } from './enums/user-role.enum';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Injectable()
 export class UserService {
@@ -34,6 +40,27 @@ export class UserService {
     return user;
   }
 
+  async createDoctor(createUserDto: CreateUserDto) {
+    const hashPass = await this.hashingService.hash(createUserDto.password);
+    const userData = {
+      username: createUserDto.username,
+      fullName: createUserDto.fullName,
+      email: createUserDto.email,
+      role: UserRoleEnum.Attendant,
+      password: hashPass,
+    };
+
+    const user: User = this.userRepository.create(userData);
+
+    if (!user) {
+      throw new HttpException('Invalid Data', HttpStatus.BAD_REQUEST);
+    }
+
+    await this.userRepository.save(user);
+
+    return user;
+  }
+
   async createMember(createUserDto: CreateUserDto) {
     const hashPass = await this.hashingService.hash(createUserDto.password);
 
@@ -56,9 +83,38 @@ export class UserService {
     return user;
   }
 
-  // findAll() {
-  //   return `This action returns all user`;
-  // }
+  async findAllDoctors(pagination: PaginationDto) {
+    const { limit = 10, offset = 0 } = pagination;
+
+    const skip = (offset - 1) * limit;
+
+    if (limit <= 0 || offset <= 0) {
+      throw new BadRequestException('Invalid pagination parameters');
+    }
+    if (limit > 50) {
+      throw new BadRequestException('Limit cannot exceed 50');
+    }
+
+    const doctors = await this.userRepository.find({
+      skip: skip,
+      take: limit,
+      where: {
+        role: UserRoleEnum.Doctor,
+      },
+    });
+
+    if (!doctors) {
+      throw new HttpException('No doctors found', HttpStatus.NOT_FOUND);
+    }
+
+    return doctors.map((doctor) => {
+      return {
+        id: doctor.id,
+        fullName: doctor.fullName,
+        email: doctor.email,
+      };
+    });
+  }
 
   async findUserById(id: string) {
     const user = await this.userRepository.findOneBy({ id });
