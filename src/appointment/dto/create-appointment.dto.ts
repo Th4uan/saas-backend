16613c8b5586f38f9ef1e83492f03f92e71e118e
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2025-05-08 14:12:02 -0300

    Vagabundo so pensa em LOL

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,7 +1,9 @@
 import { IsArray, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateAppointmentDto {
   @IsNotEmpty()
+  @Type(() => Date)  //Esse dev back ta fraco
   date: Date;
   @IsNotEmpty()
   @IsArray()
