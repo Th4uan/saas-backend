f542d2954b02ed187c107c1ba47c0f983b108c3a
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2025-05-08 14:17:10 -0300

    Vagabundo so pensa em LOL

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true, // 20 conto
     }),
   );
   app.use(cookieParser());
